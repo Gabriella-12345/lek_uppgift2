f8206dc76e34507d175c174fc17184cae9eeb466
gabriella jonsson <gabriella.jonsosn@elev.ga.ntig.se>
2023-09-18 14:31:14 +0200

    bett mottagaren att skriv en mening, skapt scanner

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner text = new scanner(System.in);
+//*Uppgift a */
+        System.out.print("Var god att skriv en mening:");
+//* uppgigt b*/
+        String ord = text.nextline();
+        
+        System.out.println("");
+    
+    }
+}
+
